df0253b3b95664803335d194771065732f5b6d38
Anil Kumawat <anilkumawat@Anils-MacBook-Air.local>
2026-08-17 22:59:06 +0530

    project setup

---
+++ src/main/java/com/jenkins/HealthController.java
@@ -0,0 +1,19 @@
+package com.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+    @GetMapping("/actuator/health")
+    public Object getHealth(){
+        return Map.of("status","UP");
+    }
+
+    @GetMapping("/api/hello")
+    public Object hello(){
+        return "Hello jenkins";
+    }
+}
